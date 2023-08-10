b9dcd49f2aceeac5093196dd622ad407f38e6d7e
binderya.ts <binderya.ts@simple.mn>
2023-08-10 09:24:03 +0800

    CombineLatest

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:rxdart_practice/views/home_page.dart';
+import 'package:rxdart/rxdart.dart';
+import 'dart:developer' as devtools show log;
+
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
 
 void main() {
   runApp(
@@ -14,6 +19,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: const HomePage(),
+    );
+  }
+}
+
+void testIt() async {
+  final stream1 = Stream.periodic(
+      const Duration(seconds: 1), (count) => 'Stream 1, count = $count');
+
+  final stream2 = Stream.periodic(
+      const Duration(seconds: 3), (count) => 'Stream 2, count = $count');
+  final combined =
+      Rx.combineLatest2(stream1, stream2, (a, b) => 'A is ($a), B is ($b)');
+
+  await for (final value in combined) {
+    value.log();
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    testIt();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
     );
   }
 }
